77bcc7595673ee4ad37e2bd961856ba39616b17d
Danyello <ulkiora.panda@gmail.com>
2021-05-28 19:43:38 +0600

    DataAccess develop version

---
+++ Persistence/Models/Category.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace ClothingStore.Models
+{
+    public partial class Category
+    {
+        public Category()
+        {
+            ProductsCategories = new HashSet<ProductsCategory>();
+        }
+        public Category(string name)
+        {
+            ProductsCategories = new HashSet<ProductsCategory>();
+            Name = name;
+        }
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public virtual ICollection<ProductsCategory> ProductsCategories { get; set; }
+    }
+}
