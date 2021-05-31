30b05879c2d528942e1864cc8cf3e23dd2494cf9
Danyello <ulkiora.panda@gmail.com>
2021-05-31 15:30:43 +0600

    чел ты...

--- frontend/layouts/empty.vue
+++
@@ -1,7 +0,0 @@
-<template>
-    <section>
-        <div class="flex mx-auto">
-            <nuxt />
-        </div>
-    </section>
-</template>
--- frontend/pages/Collection.vue
+++
@@ -1,7 +0,0 @@
-<template>
-    <section>
-        <div class="container mx-auto px-4">
-            <h1>Products page</h1>
-        </div>
-    </section>
-</template>
