82aaa480a6ade2c0e9e72ea727f105b41acd0536
BaronFenix <BaronFenix11@gmail.com>
2021-06-05 01:36:58 +0600

    page shell

---
+++ frontend/layouts/home.vue
@@ -0,0 +1,48 @@
+<template>
+    <div class="bg-home-page-fhd w-full">
+        <NavbarHome />
+        <Nuxt />
+        <Footer />
+    </div>
+</template>
+    
+<script>
+import NavbarHome from '@/components/NavbarHome'
+import Footer from '@/components/Footer'
+export default {
+components: {
+    NavbarHome,
+    Footer
+},
+computed:{
+    meta () {
+    return[
+        { charset: 'utf-8' },
+        {
+        name: 'viewport',
+        content: 'width=device-width, initial-scale=1, maximum-scale=1 shrink-to-fit=no'
+        },
+        { hid: 'description', name: 'description', content: 'Главная' }
+    ]
+    }
+},
+head() {
+    // const canonical = `https://mysite.com${this.$route.path
+    // .toLowerCase()
+    // .replace(/\/$/, '')}`
+    // return {
+    // meta: [
+    //     ...this.meta
+    // ],
+    // script: [
+    //     // { src: 'https://markknol.github.io/console-log-viewer/console-log-viewer.js' }
+    // ],
+    // link: [{ rel: 'canonical', href: canonical }]
+    // }
+}
+
+}
+</script>
+<style scoped>
+
+</style>
