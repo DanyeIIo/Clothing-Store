0c2bd7fcc0cbc7b5271c4fe2377ed0409855c9a0
BaronFenix <BaronFenix11@gmail.com>
2021-06-08 21:11:25 +0600

    need to do this thing

---
+++ frontend/pages/Login.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="w-3/6 mx-auto">
+    <h1>Регистрация</h1>
+        <form @submit.prevent="submit">
+            <input type="text" v-model="email" placeholder="email" class="bg-gray-200 p-2">
+            <input type="text" v-model="password" placeholder="password" class="bg-gray-200 p-2">
+            <button class="bg-purple-300 p-4" type="submit">Send</button>
+        </form>
+    </div>
+</template>
+
+
+<script>
+export default {
+    data() {
+        return {
+            email: '',
+            password: ''
+        }
+    },
+    methods: {
+        submimt() {
+
+        }
+    }
+}
+</script>
