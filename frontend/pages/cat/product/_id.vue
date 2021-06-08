513ecc5ec96965fc598baf5c3bfd4ff3b6a75654
BaronFenix <BaronFenix11@gmail.com>
2021-06-08 21:09:50 +0600

    added dynamic pages

---
+++ frontend/pages/cat/product/_id.vue
@@ -0,0 +1,24 @@
+<template>
+    <section>
+        <h1>dynamic Product page {{$route.params.id}} </h1>
+        
+    </section>
+</template>
+
+<script>
+export default {
+    // validate({params}){
+    //     return /^\d+$/.test(params.id)
+    // },
+    components: {
+
+    },
+    computed: mapGetters(['allItems']),
+    methods: mapActions(['fetchPosts', 'GET_ITEMS_FROM_API']),
+    async mounted(){
+        this.fetchPosts(),
+        this.GET_ITEMS_FROM_API()
+    }  
+
+}
+</script>
